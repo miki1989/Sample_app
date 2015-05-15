d290401b814e7605fe724384fcc404dce3a10242
miki89 <mikolajheller@gmail.com>
2015-05-15 23:46:25 +0200

    Make a basic User model (including secure passwords)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,5 +11,7 @@ test "layout links" do
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+	get signup_path
+	assert_select "title", full_title("Sign up")
   end
 end
