370ae6e956f30779869a7e1bed54121da95a3181
miki89 <mikolajheller@gmail.com>
2015-05-20 21:27:52 +0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ validates :email, presence: true, length: { maximum: 255 },
 has_secure_password
 validates :password, length: { minimum: 6 }, allow_blank: true
  has_many :microposts, dependent: :destroy
+has_many :active_relationships, class_name:"Relationship",
+                                  foreign_key: 						"follower_id",
+                                  dependent:   :destroy
+has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+has_many :following, through: :active_relationships, source: :followed
+
+has_many :followers, through: :passive_relationships,
+source: :follower
+
 	def User.digest(string)
 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 BCrypt::Engine.cost
@@ -58,17 +67,33 @@ BCrypt::Engine.cost
     UserMailer.password_reset(self).deliver_now
   end
 
-	 # Returns true if a password reset has expired.
-	def password_reset_expired?
-	reset_sent_at < 2.hours.ago
-	end
-
-	# Defines a proto-feed.
-  # See "Following users" for the full implementation.
-  def feed
-    Micropost.where("user_id = ?", id)
+	# Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   private
 
